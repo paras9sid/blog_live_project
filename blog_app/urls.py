a020ebf779bff116e894fa7fb854e4d3c19f3525
Sidharth Jain <sidharthjainp9@gmail.com>
2024-06-06 11:13:40 +0530

    context processors + category-post url links set

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     # <int:pk>/ or <int:category_id>/ -> same should be written in parenthesis in views - 
-    path('<int:pk>/',views.posts_by_category,name='poss_by_category'),
+    path('<int:pk>/',views.posts_by_category,name='posts_by_category'),
 ]
 
--- blog_app/views.py
+++ blog_app/views.py
@@ -4,18 +4,19 @@ from django.http import HttpResponse
 
 def home(request):
     #fetch all categories defined dynamically in admin panel
-    categories = Category.objects.all()
+    # categories = Category.objects.all() 
+    #- commented above lines as context_processors used
     # print(categories)
     
     featured_posts = Blog.objects.filter(is_featured = True,status='Published')
-    print(featured_posts)
+    # print(featured_posts)
     
     posts = Blog.objects.filter(is_featured=False).order_by('-updated_at')
     # print(posts)
     
     
     context = {
-        'categories':categories,
+        # 'categories':categories,
         'featured_posts':featured_posts,
         'posts':posts,
     }
