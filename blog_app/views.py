de182ccc429185451d7443a4ad7fcfb0cb8a7ee9
Sidharth Jain <sidharthjainp9@gmail.com>
2024-06-09 18:54:13 +0530

    login/logout implemented

--- blog_app/views.py
+++ blog_app/views.py
@@ -3,6 +3,8 @@ from assignments.models import About
 from blog_main.forms import RegistrationForm
 from .models import  Category,Blog
 from django.db.models import Q
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 def home(request):
     #fetch all categories defined dynamically in admin panel
@@ -100,3 +102,26 @@ def register(request):
     }
     return render(request,'register.html',context)
 
+def login(request):
+    
+    if request.method=='POST':
+        form = AuthenticationForm(request,request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            
+            user = auth.authenticate(username=username,password=password)
+            if user is not None:
+                auth.login(request,user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    
+    context = {
+        'form':form,
+    }
+    return render(request,'login.html',context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
