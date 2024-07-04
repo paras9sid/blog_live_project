efac07b8fa6320cc5b76892b406e28606420773e
Sidharth Jain <sidharthjainp9@gmail.com>
2024-07-04 12:50:23 +0530

    Views modified-draft posts to not render on frontened

--- blog_main/views.py
+++ blog_main/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from blog_app.models import Category,Blog
 
 def home(request):
+    
     #fetch all categories defined dynamically in admin panel
     categories = Category.objects.all()
     # print(categories)
     
     featured_posts = Blog.objects.filter(is_featured = True,status='Published')
-    print(featured_posts)
+    # print(featured_posts)
     
-    posts = Blog.objects.filter(is_featured=False).order_by('-updated_at')
+    posts = Blog.objects.filter(is_featured=False ,status='Published').order_by('-updated_at')
     # print(posts)
     
     
