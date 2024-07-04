e9b7ad3a5bf58946aec35891829ae2cb908593dc
Sidharth Jain <sidharthjainp9@gmail.com>
2024-07-04 11:52:20 +0530

    views/changed - headings in files added as additional info

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog_app import views
+from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
