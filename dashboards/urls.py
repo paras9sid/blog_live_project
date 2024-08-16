0987f354362b0980ff04aa1fea61873f750275f2
Sidharth Jain <sidharthjainp9@gmail.com>
2024-08-16 13:51:09 +0530

    urls,erropage credits update

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -11,13 +11,13 @@ urlpatterns = [
     path('categories/',views.categories,name='categories'),
     
     # crud - create/add
-    path('categories/add/',views.add_category,name='add_category'),
+    path('categories/addcategoriesNew/',views.add_category,name='add_category'),
     
     # crud - update/edit
-    path('categories/edit/<int:pk>',views.edit_category,name='edit_category'),
+    path('categories/editcategoriesOld/<int:pk>',views.edit_category,name='edit_category'),
     
     # crud - delete
-    path('categories/delete/<int:pk>',views.delete_category,name='delete_category'),
+    path('categories/deletecategoriesNewOld/<int:pk>',views.delete_category,name='delete_category'),
         
     
     # Posts - CRUD
@@ -26,26 +26,26 @@ urlpatterns = [
     path('posts/',views.posts,name='posts'),
     
     # create
-    path('posts/add/',views.add_post,name='add_post'),
+    path('posts/addpostsNew/',views.add_post,name='add_post'),
     
     # edit/update
-    path('posts/edit/<int:pk>/',views.edit_post,name='edit_post'),
+    path('posts/editpostsOld/<int:pk>/',views.edit_post,name='edit_post'),
     
     # delete
-    path('posts/delete/<int:pk>/',views.delete_post,name='delete_post'),
+    path('posts/deletepostsNewOld/<int:pk>/',views.delete_post,name='delete_post'),
     
     
     # users
     path('users/',views.users,name='users'),
 
     # add user - Create
-    path('users/add/',views.add_user,name='add_user'),
+    path('users/addusersNew/',views.add_user,name='add_user'),
     
     # Update/edit
-    path('users/edit/<int:pk>/',views.edit_user,name='edit_user'),
+    path('users/editusersOld/<int:pk>/',views.edit_user,name='edit_user'),
     
     # delete
-    path('users/delete/<int:pk>/',views.delete_user,name='delete_user'),
+    path('users/deleteusersNewOld/<int:pk>/',views.delete_user,name='delete_user'),
 
     
 ]
